cbd02f6aba1b0d5a48f3c7a9e05ab7f4959f263f
Shruti Rawul <133375866+Shrutirawul@users.noreply.github.com>
2023-09-06 21:08:47 +0530

    Add files via upload

---
+++ task 10.py
@@ -0,0 +1,6 @@
+# 1. Using python PIP command to create a new file "requirement".txt" from the terminal or command prompt
+
+pip freeze > requirements.text
+
+# 2. Using python pip install flask module < 2.0 in your system
+py -m pip install flask '''By using the command by default the latest version of flask is installed'''
